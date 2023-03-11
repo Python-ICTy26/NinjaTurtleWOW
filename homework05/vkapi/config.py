1212f9754c326756893cb57ab5b3b1027e49075f
NinjaTurtleWOW <nipa77@yandex.ru>
2023-03-12 00:29:18 +0500

    homework05 complete

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "vk1.a.UFTN6YfTQC6xL6suMVEjBZKzWnFlu5PoGa-JJ_C4zi51ns17LyVgsqyOKlDQDFGNZ-PeYeZwxmDSG0mk18bm_QuaApT8i5H9kg40Xd0ujQpnCw0PLykno3l_ohCOLvvMtSjQ_PCR6agAAkvr0hsLIcvsEDb5dt-1GJ3LvkomoDvJ10jfiDQ1VrHSofAl6nMf&expires_in=86400&user_id=337563534",
     "version": "5.126",
 }
